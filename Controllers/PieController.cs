a312aa81c5ec3cea0862ed473680f223dff57510
Carlos Duran <45047071+ndsuero@users.noreply.github.com>
2022-03-06 18:31:55 -0500

    first commit

---
+++ Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;            
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese Cake";
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
---
+++ ViewModel/PiesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.ViewModel
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
